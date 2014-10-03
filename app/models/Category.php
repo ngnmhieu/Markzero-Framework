09ca5823d08f743a25b46b4a51cb4f1ad818c7ba
ngnmhieu <ngnmhieu@gmail.com>
2014-10-03 23:42:58 +0200

    edit category

--- app/models/Category.php
+++ app/models/Category.php
@@ -4,43 +4,20 @@ use Doctrine\Common\Collections\ArrayCollection;
  * @Entity @Table(name="categories") 
  **/
 class Category extends AppModel {
+  protected static $attr_reader = ['id', 'transactions'];
+  protected static $attr_accessor = ['name', 'description'];
+
   /** @Id @Column(type="integer") @GeneratedValue **/
   protected $id;
   /** @Column(type="string") **/
   protected $name;
   /** @Column(type="string")**/
   protected $description;
-
   /** @OneToMany(targetEntity="Transaction", mappedBy="categories") **/
   protected $transactions;
 
   function __construct() {
     $this->time = new \DateTime();
-    $this->transactions = ArrayCollection();
-  }
-
-  public function getTransactions() {
-    return $this->transactions;
-  }
-
-  public function getId() {
-    return $this->id;
-  }
-
-  public function getName() {
-    return $this->name;
-  }
-
-  public function setName($name) {
-    $this->name = $name;
-  }
-
-  public function getDesc() {
-    return $this->desc;
-  }
-
-  public function setDesc($desc) {
-    $this->desc = $desc;
   }
 
   static function find($id) {

81342eaa5834d6b45b3e898e248d9b6af1be1956
ngnmhieu <ngnmhieu@gmail.com>
2014-10-12 12:19:59 +0200

    change routes

--- config/routes.php
+++ config/routes.php
@@ -17,9 +17,5 @@ App::$router->draw(function($r) {
 
   $r->map('get', '/user', 'user#index');
   $r->map('get', '/user/show/([0-9]+)', 'user#show');
-
-  $r->map('get', '/photo', 'photo#index');
-  $r->map('get', '/photo/tag/(.+)', 'photo#tag');
-  $r->map('get', '/admin/post/', 'admin/post#index');
 });
 

79a4495d40a0f0b3c43deb7a7f5d451b1f5ee313
ngnmhieu <ngnmhieu@gmail.com>
2015-05-01 21:30:06 +0200

    add more test to Response

--- test/Http/ResponseTest.php
+++ test/Http/ResponseTest.php
@@ -1,5 +1,5 @@
 <?php
-use Mockery\Http\Response;
+use Markzero\Http\Response;
 
 class ResponseTest extends \PHPUnit_Framework_TestCase {
 
@@ -12,6 +12,7 @@ class ResponseTest extends \PHPUnit_Framework_TestCase {
   }
 
   public function test_redirect_setCorrectRedirectUrl() {
+
     $path = '/book/';
     $router = \Mockery::mock('Markzero\Http\Routing\Router', array(
       'getWebpaths' => array($path)
@@ -21,10 +22,11 @@ class ResponseTest extends \PHPUnit_Framework_TestCase {
 
     $response->redirect('BookController', 'index');
 
-    $this->assertEquals($response->headers->get('Location'), $path);
+    $this->assertEquals($path, $response->headers->get('Location'));
   }
 
   public function test_redirect_setCorrectStatusCode() {
+
     $router = \Mockery::mock('Markzero\Http\Routing\Router', array(
       'getWebpaths' => array('/book/')
     ));
@@ -33,27 +35,30 @@ class ResponseTest extends \PHPUnit_Framework_TestCase {
 
     $response->redirect('BookController', 'index');
 
-    $this->assertEquals($response->getStatusCode(), Markzero\Http\Response::HTTP_FOUND);
+    $this->assertEquals(Markzero\Http\Response::HTTP_FOUND, $response->getStatusCode());
   }
 
   public function test_respond_noResponder() {
+
     $response = $this->getNewResponse(); 
 
     $response->respond();
 
-    $this->assertEquals($response->getStatusCode(), Response::HTTP_NOT_FOUND);
+    $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
   }
 
   public function test_respond_mediatypeNotSupported() {
 
     $router = \Mockery::mock('Markzero\Http\Routing\Router');
     $request = \Mockery::mock('Markzero\Http\Request', array(
-      'getAcceptableContentTypes' => array()
+      'getAcceptableContentTypes' => array('application/xml'),
+      'getFormat' => 'xml'
     ));
     $response = new Response($request, $router);
+    $response->respond_to('html', function(){});
 
     $response->respond();
-    $this->assertEquals($response->getStatusCode(), Response::HTTP_UNSUPPORTED_MEDIA_TYPE);
+    $this->assertEquals(Response::HTTP_UNSUPPORTED_MEDIA_TYPE, $response->getStatusCode());
   }
 
 }
